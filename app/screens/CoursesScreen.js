9fe278dd876f2a953bb78dee7c83f359c649d9a2
Ali Gh <aligodosi@gmail.com>
2021-04-19 01:54:55 +0430

    add card component

--- app/screens/CoursesScreen.js
+++ app/screens/CoursesScreen.js
@@ -1,13 +1,69 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, FlatList, View, Text, Image } from 'react-native'
+import Card from '../components/shared/Card'
 import Screen from '../components/shared/Screen'
+
+const courses = [
+    {
+        id: 1,
+        title: "دوره جامع NodeJs",
+        price: "300000",
+        time: "15:00:00",
+        teacher: "یونس قربانی",
+        image: require("../assets/courses/NodeJs.jpg"),
+    },
+    {
+        id: 2,
+        title: "دوره جامع ReactJs",
+        price: "200000",
+        time: "15:00:00",
+        teacher: "یونس قربانی",
+        image: require("../assets/courses/ReactJs.jpg"),
+    },
+    {
+        id: 3,
+        title: "دوره جامع ElectronJs",
+        price: "200000",
+        time: "15:00:00",
+        teacher: "یونس قربانی",
+        image: require("../assets/courses/Electron.jpg"),
+    },
+    {
+        id: 4,
+        title: "دوره جامع React Native",
+        price: "200000",
+        teacher: "یونس قربانی",
+        time: "15:00:00",
+        image: require("../assets/courses/ReactNative.jpg"),
+    },
+]
 
 
 export default function CoursesScreen() {
     return (
-        <Screen style={{ alignItems: 'center', fontSize: 25 }}>
-            <Text>صفحه کورس ها</Text>
+        <Screen style={styles.container}>
+            <FlatList
+                data={courses}
+                keyExtractor={course => course.id.toString()}
+                renderItem={({ item }) => (
+                    <Card
+                        title={item.title}
+                        time={item.time}
+                        price={item.price}
+                        image={item.image}
+                        teacher={item.teacher}
+                    />
+                )}
+            />
         </Screen>
     )
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        backgroundColor: "#f8f4f4",
+
+    }
+})
